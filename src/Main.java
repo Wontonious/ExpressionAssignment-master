a473ede1c8a42525c794addb31aa487394196d9c
Wontonious <48232238+Wontonious@users.noreply.github.com>
2019-09-17 11:25:29 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String args[]){
+        /*For the following expressions write out using code step by step how the
+        compiler will evaluate it
+        Example:
+        System.out.println(2 * 5 + 3);
+        System.out.println(10 + 3);
+        System.out.println(13);
+        Only complete one computation per line of code
+        The output for every line is always equal to the same number
+        In example above the out put should be
+        13
+        13
+        13
+        */
+        //Expression 1
+        System.out.println((90/30)+1.0);
+
+        //Expression 2
+        System.out.println(90*50/300-11.0);
+
+        //Expression 3
+        System.out.println(Math.sqrt(81)-5);
+
+        //Expression 4
+        System.out.println(45.0-(82*5/10));
+
+        //Expression 5
+        System.out.println(((500/5)+(300-100))/75.0);
+    }
+}
