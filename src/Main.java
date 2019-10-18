a9a1850a3d2b8ed226191f075b07098f59df72d7
Wontonious <snakegun12@gmail.com>
2019-10-17 21:41:02 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 public class Main {
     public static void main(String args[]){
         /*For the following expressions write out using code step by step how the
@@ -14,18 +16,43 @@ public class Main {
         13
         */
         //Expression 1
-        System.out.println((90/30)+1.0);
+        System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+        System.out.println(5+10/2-(4-8)/3);
+        System.out.println(5+10/2-(-4)/3);
+        System.out.println(5+5-(-4)/3);
+        System.out.println(5+5-(-1));
+        System.out.println(10-(-1));
+        System.out.println(11);
 
         //Expression 2
-        System.out.println(90*50/300-11.0);
+        System.out.println(23%4 + (18 - 3 / 2));
+        System.out.println(23%4 + (18 - 1));
+        System.out.println(23%4 + 17);
+        System.out.println(3 + 17);
+        System.out.println(20);
 
         //Expression 3
-        System.out.println(Math.sqrt(81)-5);
+        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        System.out.println((3 + Math.pow(3,4)/5));
+        System.out.println((3 + 81.0/5));
+        System.out.println((3 + 16.2));
+        System.out.println((19.2));
 
         //Expression 4
-        System.out.println(45.0-(82*5/10));
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + Math.pow(2,3) / 8 + (9.0 / 3));
+        System.out.println(1 - 8 + Math.pow(2,3) / 8 + 3.0);
+        System.out.println(1 - 8 + 8.0 / 8 + 3.0);
+        System.out.println(1 - 8 + 1.0 + 3.0);
+        System.out.println(-7 + 1.0 + 3.0);
+        System.out.println(-6.0 + 3.0);
+        System.out.println(-3.0);
 
         //Expression 5
-        System.out.println(((500/5)+(300-100))/75.0);
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println((7+3)*Math.sin(45)+2-Math.pow(5, 3));
+        System.out.println(10*Math.sin(45)+2-125);
+
     }
 }
